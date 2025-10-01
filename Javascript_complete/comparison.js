7a0108051e05ddb295d2fd67dfdb52d4002e2ea4
Sonu0407 <sonu.sapri2004@gmail.com>
2025-10-01 08:21:15 +0000

    strict comparison

--- Javascript_complete/comparison.js
+++ Javascript_complete/comparison.js
@@ -4,6 +4,25 @@
 // console.log("2" == 2);
 // console.log("2" != 2);  basic comparisons
 
-console.log("2" > 1);
+// console.log("2" > 1); // it is converting string 2 to number while runtime
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+
+// strict comparison ===
+
+console.log("2" === 2); // here it does't convert the datatype it checks according to given datatypes
+
+
+
 
 
