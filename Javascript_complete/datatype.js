c8cbec11786366ce5c7525289277aa746a264ff5
Sonu0407 <sonu.sapri2004@gmail.com>
2025-10-02 06:58:42 +0000

    examples of datatype and string functions

--- Javascript_complete/datatype.js
+++ Javascript_complete/datatype.js
@@ -16,18 +16,57 @@ let isloggedin = true; // boolean
 // datatypes primitive
 // number
 // bigint // it is used for bigger numbers
+// eaxmple for bigInt is bigNumber = 30824927098732408n in the end give "n" 
 // string => ""
 // boolean => true/false
 // null => standalone value
 // undefined => no value assigned
 // symbol => unique
+const id = Symbol('123')    // example for symbol datatype with symbol keyword
 
+// datatype non-primitive
 
+// array
+const heros = ["Ironman", "superman", "hulk","spiderman"]  // within brackets are called arrays
+
+// function
+const myFunction = function(){
+    console.log("Hello World")
+}  
+                                 // keyword is "function" and then parentesis and curly braces
 // object
+let object = {
+    name : "sonu",
+    age  : 21          // within curly braces are called objects
+}
+
 
 console.log(typeof(age));
 
 console.log(typeof null); // null type is an object
 
+// memory allocation
 
+// Stack (primitive datatype) Heap (non-primitive datatype)
+
+let myName = "Sonu"
+
+let anotherName = myName
+
+anotherName = "Moni"
+
+console.log(myName)
+console.log(anotherName) // in stack it gives a copy to another variable not a refrences original remain the same 
+
+let userOne = {
+    name: "sonu",
+    age: 21
+}
+
+let userTwo = userOne
+
+userTwo.name = "moni"
+
+console.log(userOne.name)
+console.log(userTwo.name) // in heap it gives the refernces variable means the original that means everything changes.
 
---
+++ Javascript_complete/strings.js
@@ -0,0 +1,37 @@
+const name  = "sonu"
+const age = 21
+
+console.log(name + age + "it's age") // dont write code like this please
+
+console.log(`My name is ${name} and I am ${age} years old`); // use this type writing code
+
+const gameName = new String('Sonu-S-com') // it is object string when new keyword
+
+console.log(typeof gameName);
+
+console.log(gameName[0]);
+
+console.log(gameName.__proto__)
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase()); // add parentesis you forget it
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('S'));
+
+console.log(gameName.split('-'));
+
+console.log(gameName.substring(0, 4));
+
+console.log(gameName.slice(-8, 4)); // in slice only we can use negative values
+
+console.log(gameName.replace('-',' '))
+
+console.log(gameName.includes('Sonu'));
+
+
+
+
+
